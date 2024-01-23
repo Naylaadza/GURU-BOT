add4bca3a914223050a068bd01dacdf030e9cf73
Naylaadza <157491631+Naylaadza@users.noreply.github.com>
2024-01-23 20:55:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['6283890842636', 'nymoss bot', true],
+  ['6285260550445', 'Nayla', true], 
   [''] 
 ] //Number of owners
 
